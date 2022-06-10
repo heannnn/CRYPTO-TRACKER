84ae92d2f851cc3a84c3603c9a0a1fa03d5d6c81
heannnn <jse033101@gmail.com>
2022-06-11 05:45:01 +0900

    process.env.PUBLIC_URL 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,7 +8,7 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Coins />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Coins />} />
         <Route path=":coinId" element={<Coin />}>
           <Route path="price" element={<Price />} />
           <Route path="chart" element={<ChartCandleStick />} />
