85f310470b37ade9b86685f0063fa15171f97ef0
heannnn <jse033101@gmail.com>
2022-06-10 23:17:01 +0900

    Nested Router

---
+++ src/Circle.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const Container = styled.div<CircleProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
+`;
+
+interface CircleProps {
+  bgColor: string; // essential
+  borderColor?: string; // optional
+  text?: string;
+}
+
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? "black"}>
+      {text}
+    </Container>
+  );
+}
+
+export default Circle;
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#273c75",
+  textColor: "#dcdde1",
+  accentColor: "#487eb0",
+};
